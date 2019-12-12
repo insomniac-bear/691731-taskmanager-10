a7ff8f0fbc10602f07d847aede5df73b39f06d51
kirill <tcherepanov.kir@gmail.com>
2019-12-12 14:22:22 +0300

    Учебный проект: разделяй и властвуй (Часть 1)

--- src/components/tasks.js
+++ src/components/tasks.js
@@ -1,4 +1,4 @@
-import {createElement} from '../utils.js';
+import AbstractComponent from './abstract-component.js';
 
 const createTaskTemplate = () => {
   return (
@@ -6,24 +6,8 @@ const createTaskTemplate = () => {
   );
 };
 
-export default class Tasks {
-  constructor() {
-    this._element = null;
-  }
-
+export default class Tasks extends AbstractComponent {
   getTemplate() {
     return createTaskTemplate();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
